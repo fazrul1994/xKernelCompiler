b768b5f4e9b94eca95c71a9cee9ab0a22e39cd83
fazrul1994 <74166146+fazrul1994@users.noreply.github.com>
2021-06-28 00:06:21 +0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -5,7 +5,8 @@
 # Warning !! Dont Change anything there without known reason.
 cd ${KERNEL_ROOTDIR}
 make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
-make -j$(nproc) ARCH=arm64 O=out \
+make menuconfig O=out 
+time make -j$(nproc) ARCH=arm64 O=out \
 	CC=${CLANG_ROOTDIR}/bin/clang \
 	CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
 	CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
